12257092047f7673f3e160f5f8d7644748b4acbb
Ron Crans <ron@rjconline.net>
2015-08-21 05:11:41 -0700

    v1.0.0

--- iossa.php
+++ iossa.php
@@ -20,24 +20,28 @@ class plgSystemIossa extends JPlugin {
 		}
 
 		$jawc = new JApplicationWebClient();
-		// if is a mobile client, add needed items to head of page
-		if ($jawc->mobile) {
+		// do nothing if not a mobile client
+		if (!$jawc->mobile) {
+			return;
+		}
 
-			// add meta and link-rels to allow creation of standalone with provided app icons
-			$doc->setMetaData('apple-mobile-web-app-capable', 'YES');
-			$doc->addHeadLink('images/apple-touch-icon.png', 'apple-touch-icon');
-			$doc->addHeadLink('images/apple-touch-icon-76x76.png', 'apple-touch-icon', 'rel', array('sizes'=>'76x76'));
-			$doc->addHeadLink('images/apple-touch-icon-120x120.png', 'apple-touch-icon', 'rel', array('sizes'=>'120x120'));
-			$doc->addHeadLink('images/apple-touch-icon-152x152.png', 'apple-touch-icon', 'rel', array('sizes'=>'152x152'));
+		// add needed items to head of page
+		// add meta and link-rels to allow creation of standalone with provided app icons
+		$doc->setMetaData('apple-mobile-web-app-capable', 'YES');
+		$doc->addHeadLink('images/apple-touch-icon.png', 'apple-touch-icon');
+		$doc->addHeadLink('images/apple-touch-icon-76x76.png', 'apple-touch-icon', 'rel', array('sizes'=>'76x76'));
+		$doc->addHeadLink('images/apple-touch-icon-120x120.png', 'apple-touch-icon', 'rel', array('sizes'=>'120x120'));
+		$doc->addHeadLink('images/apple-touch-icon-152x152.png', 'apple-touch-icon', 'rel', array('sizes'=>'152x152'));
 
-			// modify every anchor link to keep from transferring to Safari when activated
-			$so_script = 'var iWebkit;
-if (!iWebkit) {
-	iWebkit = jQuery(document).ready( function() {
+		// modify every anchor link to keep from transferring to Safari when activated
+		$so_script = 'var iossaWebkit;
+if (!iossaWebkit) {
+	iossaWebkit = jQuery(document).ready( function() {
 			function fullscreen() {
 				var a=document.getElementsByTagName("a");
 				for (var i=0; i<a.length; i++) {
-					if (a[i].className.match("noeffect") || (a[i].getAttribute("href").substring(0, 11) == "javascript:")) {
+					var hrf = a[i].getAttribute("href");
+					if ((hrf.substring(0, 1) == "#") || (hrf.substring(0, 11) == "javascript:") || a[i].className.match("noeffect")) {
 					} else {
 						a[i].onclick = function() {
 								window.location = this.getAttribute("href");
@@ -46,15 +50,14 @@ if (!iWebkit) {
 					}
 				}
 			}
-			iWebkit.init = function() {
+			iossaWebkit.init = function() {
 					fullscreen();
 				};
-			iWebkit.init();
+			iossaWebkit.init();
 		});
 }
 ';
-			$doc->addScriptDeclaration($so_script);
-		}
+		$doc->addScriptDeclaration($so_script);
 	}
 
 }
