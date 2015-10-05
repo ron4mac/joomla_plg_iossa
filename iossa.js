64d9b930fc103b274c110fdbb8ad776ebeeb4d91
Ron Crans <ron@rjconline.net>
2015-10-05 06:19:22 -0700

    v1.0.5

---
+++ iossa.js
@@ -0,0 +1,95 @@
+var iosSA;
+if (!iosSA) {
+	iosSA = jQuery(document).ready( function() {
+		function fullscreen() {
+			var a = document.getElementsByTagName("a");
+			for (var i=0; i<a.length; i++) {
+					if (!a[i].hasAttribute("href")) {
+						console.log(a[i]);
+					}
+				if (a[i].className.match("noeffect") || (a[i].hasAttribute("href") && a[i].getAttribute("href").substring(0, 11) == "javascript:")) {
+				} else {
+					a[i].onclick = function() {
+							window.location = this.getAttribute("href");
+							return false;
+						};
+				}
+			}
+		}
+
+		iosSA.startX = 0;
+		iosSA.startY = 0;
+		iosSA.fingerCount = 0;
+		iosSA.curX = 0;
+		iosSA.curY = 0;
+		iosSA.swipe = 0;
+
+		iosSA.touchStart = function (event) {
+			// disable the standard ability to select the touched object
+		//	event.preventDefault();
+			// get the total number of fingers touching the screen
+			iosSA.fingerCount = event.touches.length;
+			// since we are looking for a swipe (single finger) and not a gesture (multiple fingers),
+			// check that only one finger was used
+			if ( iosSA.fingerCount == 1 ) {
+				// get the coordinates of the touch
+				iosSA.startX = event.touches[0].pageX;
+				iosSA.startY = event.touches[0].pageY;
+			} else {
+				// more than one finger touched so cancel
+				iosSA.touchCancel(event);
+			}
+		};
+
+		iosSA.touchEnd = function (event) {
+		//	event.preventDefault();
+			// check to see if more than one finger was used and that there is an ending coordinate
+			if (iosSA.fingerCount == 1 && iosSA.curX !== 0) {
+				var xD = iosSA.curX - iosSA.startX;
+				var absY = Math.abs(iosSA.curY - iosSA.startY);
+				if (!absY) absY = 1; 
+				var dXY = xD / absY;
+				if (dXY > 4 && xD > 300) {
+					//document.getElementById("system-message-container").innerHTML = "RIGHT";
+					window.history.back();
+				} else if (dXY < 4 && xD < 300) {
+					//document.getElementById("system-message-container").innerHTML = "LEFT";
+					window.history.forward();
+				}
+			} else {
+				iosSA.touchCancel(event);
+			}
+		};
+
+		iosSA.touchMove = function (event) {
+		//	event.preventDefault();
+			if (event.touches.length == 1) {
+				iosSA.curX = event.touches[0].pageX;
+				iosSA.curY = event.touches[0].pageY;
+			} else {
+				iosSA.touchCancel(event);
+			}
+		};
+
+		iosSA.touchCancel = function (event) {
+			iosSA.startX = 0;
+			iosSA.startY = 0;
+			iosSA.fingerCount = 0;
+			iosSA.curX = 0;
+			iosSA.curY = 0;
+			iosSA.swipe = 0;
+		};
+
+		iosSA.init = function () {
+				fullscreen();
+				if (window.navigator.standalone) {
+					window.addEventListener("touchstart", function(){ iosSA.touchStart(event); });
+					window.addEventListener("touchend", function(){ iosSA.touchEnd(event); });
+					window.addEventListener("touchmove", function(){ iosSA.touchMove(event); });
+					window.addEventListener("touchcancel", function(){ iosSA.touchCancel(event); });
+				}
+			};
+
+		iosSA.init();
+	});
+}
--- iossa.php
+++ iossa.php
@@ -5,7 +5,7 @@ jimport('joomla.plugin.plugin');
 
 class plgSystemIossa extends JPlugin {
 
-	function onAfterInitialise() {
+	public function onAfterInitialise() {
 
 		$app = JFactory::getApplication();
 		// do nothing if in the admin
@@ -20,44 +20,19 @@ class plgSystemIossa extends JPlugin {
 		}
 
 		$jawc = new JApplicationWebClient();
-		// do nothing if not a mobile client
-		if (!$jawc->mobile) {
-			return;
+		// if is a mobile client, add needed items to head of page
+		if ($jawc->mobile) {
+
+			// add meta and link-rels to allow creation of standalone with provided app icons
+			$doc->setMetaData('apple-mobile-web-app-capable', 'YES');
+			$doc->addHeadLink('images/apple-touch-icon.png', 'apple-touch-icon');
+			$doc->addHeadLink('images/apple-touch-icon-76x76.png', 'apple-touch-icon', 'rel', array('sizes'=>'76x76'));
+			$doc->addHeadLink('images/apple-touch-icon-120x120.png', 'apple-touch-icon', 'rel', array('sizes'=>'120x120'));
+			$doc->addHeadLink('images/apple-touch-icon-152x152.png', 'apple-touch-icon', 'rel', array('sizes'=>'152x152'));
+
+			// add our javascript functions
+			$doc->addScriptDeclaration(file_get_contents(JPATH_ROOT.'/plugins/system/iossa/iossa.js'));
 		}
-
-		// add needed items to head of page
-		// add meta and link-rels to allow creation of standalone with provided app icons
-		$doc->setMetaData('apple-mobile-web-app-capable', 'YES');
-		$doc->addHeadLink('images/apple-touch-icon.png', 'apple-touch-icon');
-		$doc->addHeadLink('images/apple-touch-icon-76x76.png', 'apple-touch-icon', 'rel', array('sizes'=>'76x76'));
-		$doc->addHeadLink('images/apple-touch-icon-120x120.png', 'apple-touch-icon', 'rel', array('sizes'=>'120x120'));
-		$doc->addHeadLink('images/apple-touch-icon-152x152.png', 'apple-touch-icon', 'rel', array('sizes'=>'152x152'));
-
-		// modify every anchor link to keep from transferring to Safari when activated
-		$so_script = 'var iossaWebkit;
-if (!iossaWebkit) {
-	iossaWebkit = jQuery(document).ready( function() {
-			function fullscreen() {
-				var a=document.getElementsByTagName("a");
-				for (var i=0; i<a.length; i++) {
-					var hrf = a[i].getAttribute("href");
-					if ((hrf.substring(0, 1) == "#") || (hrf.substring(0, 11) == "javascript:") || a[i].className.match("noeffect")) {
-					} else {
-						a[i].onclick = function() {
-								window.location = this.getAttribute("href");
-								return false;
-							}
-					}
-				}
-			}
-			iossaWebkit.init = function() {
-					fullscreen();
-				};
-			iossaWebkit.init();
-		});
-}
-';
-		$doc->addScriptDeclaration($so_script);
 	}
 
 }
